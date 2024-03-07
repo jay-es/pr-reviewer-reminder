dfc662645a48f03caea4ee55c906719f9efe5901
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2024-03-08 01:19:47 +0900

    App layout

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,62 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1 class="p-4 text-center text-3xl">PR Reviewer Reminder</h1>
   </header>
 
-  <main>
-    <TheWelcome />
+  <main class="mx-auto w-full max-w-sm px-4">
+    <form class="flex flex-col gap-y-4" @submit.prevent>
+      <label class="form-control">
+        <div class="label">
+          <span class="label-text">GitHub account</span>
+        </div>
+        <input
+          type="text"
+          class="input input-sm input-bordered invalid:input-error"
+          placeholder="input your account"
+        />
+      </label>
+
+      <label class="form-control">
+        <div class="label">
+          <span class="label-text">
+            owner/repo
+            <span class="text-muted label-text-alt">
+              (accepts multiple values by line breaks)
+            </span>
+          </span>
+        </div>
+        <textarea
+          class="textarea textarea-bordered textarea-sm h-24 leading-normal invalid:textarea-error"
+          :placeholder="`nodejs/node\nmicrosoft/TypeScript`"
+        ></textarea>
+      </label>
+
+      <label class="form-control">
+        <div class="label">
+          <span class="label-text">
+            personal access token
+            <span class="text-muted label-text-alt">(for private repos)</span>
+          </span>
+        </div>
+        <input
+          type="text"
+          class="input input-sm input-bordered invalid:input-error"
+        />
+      </label>
+
+      <button class="btn btn-primary btn-sm mt-4">Check now</button>
+    </form>
   </main>
+
+  <footer class="p-3 text-center">
+    <a
+      href="https://github.com/jay-es/pr-reviewer-reminder"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      &copy;<span class="link">jay-es</span>
+    </a>
+  </footer>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+/* eslint-env node */
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        muted: '#656d76',
+      },
+    },
   },
-  plugins: [require('daisyui')]
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#1f883d',
+          'primary-content': '#fff',
+          secondary: '#0969da',
+          'base-100': '#ffffff',
+          'base-content': '#1f2328',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+};
